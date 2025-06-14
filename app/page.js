d3d5ee43f7632e5fb0bed8b94ba85813cf2282c5
David <david.diaz.diaz.yt@gmail.com>
2025-06-14 15:08:58 +0200

    feat: landing page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,18 @@ export default function Home() {
   const name = "David";
   return (
     <main>
+      <section className="bg-base-200">
+        <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+          <div>CodeFastSaas</div>
+          <div className="space-x-4">
+            <a className="link link-hover">Pricing</a>
+            <a className="link link-hover">FAQ</a>
+          </div>
+          <div>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          </div>
+        </div>
+      </section>
       <section className="px-8 text-center py-32 max-w-3xl mx-auto">
         <h1 className="text-4xl font-extrabold mb-6">
           Collect customer feedback to build better products
